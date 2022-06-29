cec0216c7f2b8e5b07978116c07559ecc7f53a49
Greg <gsteen@soos.io>
2022-06-29 13:32:24 -0400

    con't

--- src/files/usr/soos/run-soos-dast.sh
+++ src/files/usr/soos/run-soos-dast.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 echo .
-read -p "Enter your SOOS Client Id (found here https://app.soos.io/integrate/dast): " clientId
-read -p "Enter your SOOS API Key (found here https://app.soos.io/integrate/dast): " apiKey
+read -p "Paste your SOOS Client Id (found here https://app.soos.io/integrate/dast): " clientId
+read -p "Paste your SOOS API Key (found here https://app.soos.io/integrate/dast): " apiKey
 read -p "Enter base URL of the site to scan (a full https URL only): " targetUrl
 read -p "Enter the project name to use on SOOS (e.g. 'My Website'): " projectName
-read -e -p "Enter the type of scan you would like to run (baseline, fullscan, or apiscan):" -i "baseline" scanMode
-read -e -p "Vulnerability Level (PASS, IGNORE, INFO, WARN or FAIL):" -i "PASS" level
-read -e -p "Enter the SOOS API url:" -i "https://api.soos.io/api/" apiURL
+read -e -p "Enter the type of scan you would like to run (baseline, fullscan, or apiscan): " -i "baseline" scanMode
+read -e -p "Vulnerability Level (PASS, IGNORE, INFO, WARN or FAIL): " -i "PASS" level
+read -e -p "Enter the SOOS API url: " -i "https://api.soos.io/api/" apiURL
 
-docker run -it --rm soosio/dast --clientId=$clientId --apiKey=$apiKey --projectName=$projectName --scanMode=$scanMode --ajaxSpider=true --level=$level --apiURL=$apiURL $targetUrl
+docker run -it --rm soosio/dast --clientId=$clientId --apiKey=$apiKey --projectName="$projectName" --scanMode=$scanMode --ajaxSpider=true --level=$level --apiURL=$apiURL $targetUrl
